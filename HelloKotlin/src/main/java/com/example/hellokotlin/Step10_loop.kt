61729727ce450e038aecd1b5b143ae7689319a41
가현 <bufar1014@gmail.com>
2023-08-01 10:50:48 +0900

    Step10_loop added

--- HelloKotlin/src/main/java/com/example/hellokotlin/Step10_loop.kt
+++ HelloKotlin/src/main/java/com/example/hellokotlin/Step10_loop.kt
@@ -8,4 +8,33 @@ fun main() {
     for (item in names) {
         println(item)
     }
+
+    println("--------")
+    // index가 필요한 경우
+    for (index in names.indices) {      // names 목록의 방 번호로만 이루어진 목록
+        println("$index 번째 인덱스 : ${names[index]}")
+    }
+
+    println("--------")
+    // 1 ~ 10 출력
+    for (num in 1..10) {
+        println(num)
+    }
+
+    println("--------")
+    for(num in 1..10 step 2) {
+        println(num)
+    }
+
+    println("--------")
+    // 10 ~ 1
+    for (num in 10 downTo 1) {
+        println(num)
+    }
+
+    println("--------")
+    // names 배열의 아이템을 역순ㄴ으로 참조
+    for (index in names.size-1 downTo 0) {
+        println("$index 번째 인덱스 : ${names[index]}")
+    }
 }

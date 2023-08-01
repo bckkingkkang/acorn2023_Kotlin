f84398d949398ed79bd0d2ec738cabe36b302d80
가현 <bufar1014@gmail.com>
2023-08-01 10:11:26 +0900

    Step08_useJava added

--- HelloKotlin/src/main/java/com/example/hellokotlin/java/MemberDto.java
+++ HelloKotlin/src/main/java/com/example/hellokotlin/java/MemberDto.java
@@ -7,5 +7,34 @@ public class MemberDto {
 
     public MemberDto() {}
 
+    public MemberDto(int num, String name, String addr) {
+        super();
+        this.num = num;
+        this.name = name;
+        this.addr = addr;
+    }
 
+    public int getNum() {
+        return num;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddr() {
+        return addr;
+    }
+
+    public void setAddr(String addr) {
+        this.addr = addr;
+    }
 }
